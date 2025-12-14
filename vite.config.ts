269d428284818a3aa37b4a2f1f0d403a9f8385cf
bluedream1831-collab <bluedream1831@gmail.com>
2025-12-14 09:00:25 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import process from 'process';
+import process from 'node:process';
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
