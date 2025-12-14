237a4e30599e3a469717dda3e969c5b9dd873b89
bluedream1831-collab <bluedream1831@gmail.com>
2025-12-14 08:53:04 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,15 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import process from 'process';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      // Polyfill process.env.API_KEY for the frontend code to work smoothly
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  };
 });
